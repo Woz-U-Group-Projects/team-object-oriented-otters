584e2aadff8b73d42ab929e261d5f3b7ed1d1713
DCH-CH <diedrich.chavarria@yahoo.com>
2021-04-19 20:28:21 -0700

    Just added an endpoint

---
+++ server-java-mongodb/src/main/java/com/plateit/app/controllers/UnitTypeController.java
@@ -0,0 +1,32 @@
+package com.plateit.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.plateit.app.models.UnitType;
+import com.plateit.app.models.UnitTypeRepository;
+
+@RestController
+@RequestMapping("/")
+public class UnitTypeController {
+	
+
+	@Autowired
+	UnitTypeRepository unitTypeRepository;
+	
+	
+	@PostMapping()
+	public UnitType createUnitType(@RequestBody UnitType unitType) {
+		return unitTypeRepository.save(unitType);
+	}
+	
+
+}
+
---
+++ server-java-mongodb/src/main/java/com/plateit/app/models/UnitTypeRepository.java
@@ -0,0 +1,8 @@
+package com.plateit.app.models;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UnitTypeRepository extends MongoRepository<UnitType, String>{
+
+	
+}

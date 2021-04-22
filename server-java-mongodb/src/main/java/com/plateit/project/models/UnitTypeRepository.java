b4fb6f88a1fadbf251950b69126df817a00c8efb
alexander scharp <alexscharp2002@gmail.com>
2021-04-21 20:31:47 -0500

    Created UnitType.class UnitTypeRepository.class and UnitTypecontroller

---
+++ server-java-mongodb/src/main/java/com/plateit/project/models/UnitTypeRepository.java
@@ -0,0 +1,8 @@
+package com.plateit.project.models;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UnitTypeRepository extends MongoRepository<UnitType, String>{
+
+	
+}
